351fb6453917b80ec22b93b5162c26edfc65116d
unknown <imprashantmodi21@gmail.com>
2024-08-06 16:41:37 +0530

    Reviews Fixed

--- Client/src/components/Restaurants.jsx
+++ Client/src/components/Restaurants.jsx
@@ -10,7 +10,7 @@ const Restaurants = ({item: {_id, image, name, cuisines, rating}}) => {
             </div>
             <div className='flex justify-between items-center'>
               <h4 className='text-lg font-semibold'>{name}</h4>
-              <div className={`flex items-center space-x-1 font-semibold bg-green-${(Math.round(rating)+2)*100} rounded-lg text-white text-xs p-1`}><span>{rating}</span><i className="fa-solid fa-star"></i></div>
+              <div className={`flex items-center space-x-1 font-semibold ${rating >= 4 ? `bg-green-700` : rating >= 3 ? `bg-green-600` : `bg-yellow-500`} rounded-lg text-white text-xs p-1`}><span>{rating}</span><i className="fa-solid fa-star"></i></div>
             </div>
             <div className='font-thin text-sm text-gray-500'>
               <span>{cuisines.toString().split(", ")}</span>
